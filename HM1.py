512747ac98d0ffc47a318a4635e1e6ebe10b5814
Dimitr88 <reznitchenko1505@gmail.com>
2022-12-02 13:32:35 +0200

    First HomeWork

---
+++ HM1.py
@@ -0,0 +1,20 @@
+first_string = '\timport antigravity\n\timport __hello__\n\tfrom __future__ import braces'
+print(first_string)
+
+my_name = '''
+**********     *           *  *           *  ***********    **********        ******
+*          *   **         **   *         *        *         *          *   *          *
+*           *  * *       * *    *       *         *         *          *  *            *
+*           *  *  *     *  *     *     *          *         *          *  *            *
+*           *  *   *   *   *      *   *           *         ***********   *            *
+*           *  *    * *    *       * *            *         *          *  *            *
+*           *  *           *        *             *         *           * *            *
+*          *   *           *        *             *         *           *  *          *
+**********     *           *        *             *         *           *     ******  
+'''
+print(my_name)
+
+escape_sequences = '\\a  \tBell (alert)\n\\b  \tBackspace\n\\n  \tNew line ' \
+    '\n\\t  \tHorizontal tab\n\\\  \tBackslash\n\\"  \tDouble quotation mark " ' \
+    "\n\\'  \tDouble quotation mark'"
+print(escape_sequences)
