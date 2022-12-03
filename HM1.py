d7a10d7afd59a288ac997de704904bb2a0de31d7
Dimitr88 <reznitchenko1505@gmail.com>
2022-12-03 11:41:05 +0200

    4th task with escape sequences

--- HM1.py
+++ HM1.py
@@ -1,17 +1,15 @@
 first_string = '\timport antigravity\n\timport __hello__\n\tfrom __future__ import braces'
 print(first_string)
 
-my_name = '''
-**********     *           *  *           *  ***********    **********        ******
-*          *   **         **   *         *        *         *          *   *          *
-*           *  * *       * *    *       *         *         *          *  *            *
-*           *  *  *     *  *     *     *          *         *          *  *            *
-*           *  *   *   *   *      *   *           *         ***********   *            *
-*           *  *    * *    *       * *            *         *          *  *            *
-*           *  *           *        *             *         *           * *            *
-*          *   *           *        *             *         *           *  *          *
-**********     *           *        *             *         *           *     ******  
-'''
+my_name ='*'*10+"    "+"*"+"\t\t\t  *"+"\t *"+"\t\t\t *"+"  "+'*'*11+"  "+'*'*10+"\t\t   "+"******"\
+"\n*\t\t  *\t  **\t\t **\t  *\t\t\t*\t\t *\t\t *\t\t\t*\t*\t\t   *" \
+"\n*\t\t   *  * *\t\t* *\t   *\t   *\t\t *\t\t *\t\t\t*  *\t\t\t*" \
+"\n*\t\t   *  *  *     *  *\t\t*\t  *\t\t\t *\t\t *\t\t\t*  *\t\t\t*" \
+"\n*\t\t   *  *   *   *   *\t\t *\t *\t\t\t *"+"\t\t "+'*'*11+"   *\t\t\t*" \
+"\n*\t\t   *  *    * *    *\t\t  * *\t\t\t *\t\t *\t\t\t*  *\t\t\t*" \
+"\n*\t\t   *  *\t\t\t  *\t\t   *\t\t\t *\t\t *\t\t\t * *\t\t\t*" \
+"\n*\t\t  *   *\t\t\t  *\t\t   *\t\t\t *\t\t *\t\t\t *  *\t\t   *"\
+'\n'+'*'*10+"\t  *\t\t\t  *\t\t   *\t\t\t *\t\t *\t\t\t *\t   ******"
 print(my_name)
 
 escape_sequences = '\\a  \tBell (alert)\n\\b  \tBackspace\n\\n  \tNew line ' \
