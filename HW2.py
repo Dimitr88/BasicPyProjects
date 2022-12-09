250012913d94521b3494e4ceda44c693d284a4b1
Dimitr88 <reznitchenko1505@gmail.com>
2022-12-09 12:47:33 +0200

    HW2

---
+++ HW2.py
@@ -0,0 +1,28 @@
+#Task 1
+apples = int(input('Enter number of apples: '))
+pupils = int(input('Enter number of pupils: '))
+print(f'Number of apples for each pupil: {apples} / {pupils} = {apples // pupils}')
+print(f'Number of apples in the backet: {apples} % {pupils} = {apples % pupils}')
+
+#Task 2
+first_class = int(input('Enter number of pupils for the first class: '))
+second_class = int(input('Enter number of pupils for the second class: '))
+third_class = int(input('Enter number of pupils for the third: '))
+print(f'required number of school desks: ({first_class //2} + {first_class %2} = {first_class // 2 + first_class % 2})'
+      f' + ({second_class //2} + {second_class %2} = {second_class // 2 + second_class % 2})'
+      f' + ({third_class //2} + {third_class %2} = {third_class // 2 + third_class % 2})'
+      f' = {first_class // 2 + first_class % 2 + second_class // 2 + second_class % 2 + third_class // 2 + third_class % 2}')
+
+#Task 3
+N = int(input('Enter integer: '))
+print(f'{N%10}'
+      f'{N//10%10}'
+      f'{N//100}')
+
+#Task 4
+N = int(input('Enter integer: '))
+Hours = N//3600
+S = N%3600
+Minutes = S//60
+Seconds = N%60
+print(Hours,str(':'),Minutes,str(':'),Seconds)
