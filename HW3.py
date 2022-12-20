c39decadbdca23d8dd6dbbe73ed4a8364c9f5efb
Dimitr88 <reznitchenko1505@gmail.com>
2022-12-20 18:30:19 +0200

    HW2 Task4 rounded hours up to 24

---
+++ HW3.py
@@ -0,0 +1,37 @@
+#Task 1
+first_operand = int(input('Enter first integer: '))
+operator = input('Enter operator: ')
+second_operand = int(input('Enter second integer: '))
+
+if operator == '/':
+    print(f'result: {first_operand}/{second_operand} = {first_operand/second_operand}')
+elif operator == '*':
+    print(f'result: {first_operand}*{second_operand} = {first_operand*second_operand}')
+elif operator == '+':
+    print(f'result: {first_operand}+{second_operand} = {first_operand+second_operand}')
+elif operator == '-':
+    print(f'result: {first_operand}-{second_operand} = {first_operand-second_operand}')
+
+#Task2
+N = int(input('Enter integer: '))
+for i in range (1,N+1):
+    if i**2<= N:
+        print(i**2,end = ' ')
+print()
+#Task3
+
+Number = int(input('Enter: '))
+x=0
+for i in range(1, Number+1):
+   if Number%i==0:
+    x+=1
+if x==2:
+    print(Number, 'is a prime')
+elif x > 2:
+    print (Number, 'is not a prime')
+else:
+    print ('Error')
+
+#Task4 in progress, have no time
+
+
